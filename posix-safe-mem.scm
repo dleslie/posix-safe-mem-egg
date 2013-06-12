0b1a1551586231824134197bc3291d950d087daa
Dan Leslie <dan@ironoxide.ca>
2013-06-11 18:11:55 -0700

    Added a lock count to allow multiple nested locking in a single process

--- posix-safe-mem.scm
+++ posix-safe-mem.scm
@@ -1,32 +1,43 @@
-(module posix-safe-mem
-        *
+(module posix-safe-mem *
 
         (import scheme chicken)
         (use posix lolevel posix-shm posix-semaphore uuid)
 
         ;; Store PSHM object
         (define-record safe-mem 
-          semaphore semaphore-name memory-size shared-memory shared-memory-path memory-map copy)
+          semaphore semaphore-name memory-size shared-memory shared-memory-path memory-map copy owner-pid pass-count)
 
         (define-record-printer (safe-mem m p)
           (fprintf p "#,<safe-mem semaphore: ~S semaphore-name: ~S memory-size: ~S shared-memory: ~S shared-memory-path: ~S memory-map: ~S copy: ~S>"
                    (safe-mem-semaphore m) (safe-mem-semaphore-name m) (safe-mem-memory-size m) (safe-mem-shared-memory m) (safe-mem-shared-memory-path m) (safe-mem-memory-map m) (safe-mem-copy m)))
 
+        (define (lock-safe-mem safe-mem)
+          (when (not (eq? (current-process-id) (safe-mem-owner-pid safe-mem)))
+                (sem-wait (safe-mem-semaphore safe-mem))
+                (safe-mem-owner-pid-set! safe-mem (current-process-id)))
+          (safe-mem-pass-count-set! safe-mem (+ 1 (safe-mem-pass-count safe-mem))))
+
+        (define (unlock-safe-mem safe-mem)
+          (when (eq? (current-process-id) (safe-mem-owner-pid safe-mem))
+                (safe-mem-pass-count-set! safe-mem (+ 1 (safe-mem-pass-count safe-mem)))
+                (when (eq? 0 (safe-mem-pass-count safe-mem))
+                      (safe-mem-owner-pid-set! safe-mem #f)
+                      (sem-post (safe-mem-semaphore safe-mem)))))
+
         ;; Copy to/from PSHM
+        ;; Second semaphore, inc on passing first, dec on done, if can't dec, then post first.
         (define-syntax with-safe-mem
           (syntax-rules ()
             ((with-safe-mem () body1 body2 ...)
              (begin body1 body2 ...))
-            ;; ((with-safe-mem safe-mem body1 body2 ...)
-            ;;  (with-safe-mem (safe-mem) body1 body2 ...))
             ((with-safe-mem (safe-mem1 safe-mem2 ...) body1 body2 ...)
              (dynamic-wind
                  (lambda ()
-                   (sem-wait (safe-mem-semaphore safe-mem1)))
+                   (lock-safe-mem safe-mem1))
                  (lambda ()
                    (with-safe-mem (safe-mem2 ...) body1 body2 ...))
                  (lambda ()
-                   (sem-post (safe-mem-semaphore safe-mem1)))))))
+                   (unlock-safe-mem safe-mem1))))))
 
         ;; get: lambda returns value, using unevict if copy and pointer->object if not
         (define (safe-mem-get safe-mem)
@@ -62,8 +73,8 @@
         (define make-safe-mem
           (let ((%make make-safe-mem))
             (lambda (value 
-                     #!key (semaphore-name (string-concatenate (list "/safe-mem-semaphore-" (uuid-v4))))
-                     (shared-memory-name (string-concatenate (list "/safe-mem-shared-memory-" (uuid-v4))))
+                     #!key (semaphore-name (sprintf "/safe-mem-semaphore-~A" (uuid-v4)))
+                     (shared-memory-name (sprintf "/safe-mem-shared-memory-~A" (uuid-v4)))
                      (size (object-size value))
                      (copy #t))
               (let ((safe-mem (%make (sem-open/mode semaphore-name o/creat 0644 1)
@@ -72,7 +83,10 @@
                                      (shm-open shared-memory-name (list open/creat open/rdwr))
                                      shared-memory-name
                                      #f
-                                     copy)))
+                                     copy
+                                     #f
+                                     0
+                                     )))
                 (safe-mem-set! safe-mem value)
                 safe-mem))))
 
@@ -92,10 +106,12 @@
           (with-safe-mem 
            (a b)
            (if (safe-mem-copy a)
-               (let* ((t (safe-mem-get a)))
-                 (safe-mem-set! a (safe-mem-get b))
+               (let* ((t (safe-mem-get a))
+                      (t2 (safe-mem-get b)))
+                 (safe-mem-set! a (if (safe-mem-copy b) t2 (object-evict t2)))
                  (safe-mem-set! b t))
-               (let* ((t (object-evict (safe-mem-get a))))
-                 (safe-mem-set! a (safe-mem-get b))
+               (let* ((t (object-evict (safe-mem-get a)))
+                      (t2 (safe-mem-get b)))
+                 (safe-mem-set! a (if (safe-mem-copy b) t2 (object-evict t2)))
                  (safe-mem-set! b t)))))
 )
